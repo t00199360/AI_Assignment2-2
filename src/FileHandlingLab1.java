db2c0d3bdbd93c8042758cda9b7bde38ecf2a9c7
t00199360 <t00199360@gmail.com>
2019-09-15 14:04:43 +0100

    14/09/19 + Added the timer to the get LinkedList method + Added the results to the Lab Sheet (Saved to USB) 15/09/19 + Added the remaining methods and updated the Lab sheets results

--- src/FileHandlingLab1.java
+++ src/FileHandlingLab1.java
@@ -42,29 +42,75 @@ public class FileHandlingLab1
         System.out.println("size of file: " + size);
 
         long startTimeAList = System.nanoTime();
-        testGetForArrayList(myAList);
+        testGetForArrayList(myAList);       //GetArrayList
         long endTimeAList = System.nanoTime();
         long duration = (endTimeAList - startTimeAList);
         System.out.println("The duration of the method get ArrayList in milliseconds is: " + duration/1000000);
 
-
         long startTime = System.nanoTime();
-        testGetForLinkedList(myLList);
+        testGetForLinkedList(myLList);      //GetLinkedList
         long endTime = System.nanoTime();
 
         long durationLinkedList = (endTime - startTime);
         System.out.println("The duration of the method get LinkedList in milliseconds is: " + durationLinkedList/1000000);
 
+
+
+        long startTimeSetArrayList = System.nanoTime();
+        testSetForList(myAList);                //SetArrayList
+        long endTimeSetArrayList = System.nanoTime();
+
+        long durationSetArrayList = (endTimeSetArrayList - startTimeSetArrayList);
+        System.out.println("The duration of the method set ArrayList in milliseconds is: " + durationSetArrayList/1000000);
+
+        long startTimeSetLinkedList = System.nanoTime();
+        testSetForList(myLList);                //SetLinkedList
+        long endTimeSetLinkedList = System.nanoTime();
+
+        long durationSetLinkedList = (endTimeSetLinkedList - startTimeSetLinkedList);
+        System.out.println("The duration of the method set LinkedList in milliseconds is: " + durationSetLinkedList/1000000);
+
+
+        long startTimeInsertArrayList = System.nanoTime();
+        testInsertForList(myAList);                //InsertArrayList
+        long endTimeInsertArrayList = System.nanoTime();
+
+        long durationInsertArrayList = (endTimeInsertArrayList - startTimeInsertArrayList);
+        System.out.println("The duration of the method Insert ArrayList in milliseconds is: " + durationInsertArrayList/1000000);
+
+        long startTimeInsertLinkedList = System.nanoTime();
+        testInsertForList(myLList);                //InsertLinkedList
+        long endTimeInsertLinkedList = System.nanoTime();
+
+        long durationInsertLinkedList = (endTimeInsertLinkedList - startTimeInsertLinkedList);
+        System.out.println("The duration of the method Insert LinkedList in milliseconds is: " + durationInsertLinkedList/1000000);
+
+        long startTimeRemoveArrayList = System.nanoTime();
+        testRemoveForList(myAList);                //RemoveArrayList
+        long endTimeRemoveArrayList = System.nanoTime();
+
+        long durationRemoveArrayList = (endTimeRemoveArrayList - startTimeRemoveArrayList);
+        System.out.println("The duration of the method Remove ArrayList in milliseconds is: " + durationRemoveArrayList/1000000);
+
+        long startTimeRemoveLinkedList = System.nanoTime();
+        testRemoveForList(myLList);                //removeLinkedList
+        long endTimeRemoveLinkedList = System.nanoTime();
+
+        long durationRemoveLinkedList = (endTimeRemoveLinkedList - startTimeRemoveLinkedList);
+        System.out.println("The duration of the method remove LinkedList in milliseconds is: " + durationRemoveLinkedList/1000000);
+
     }
-    public static void testGetForArrayList (ArrayList list)
+
+    private static void testGetForArrayList(ArrayList list)
     {
         testGetForList(list);
     }
-    public static void testGetForLinkedList (LinkedList list)
+    private static void testGetForLinkedList(LinkedList list)
     {
         testGetForList(list);
     }
-    public static void testGetForList(List list)
+
+    private static void testGetForList(List list)
     {
         int listSize = list.size();
         Random random = new Random();
@@ -75,6 +121,26 @@ public class FileHandlingLab1
             list.get(random.nextInt(listSize)); // list is your list
             // (ArrayList or LinkedList)
         }
+    }
+    private static void testInsertForList(List list)
+    {
+        int reps= 50000;
+        for (int i = 0; i< reps; i++)
+            list.add(5, "Test");    //minimise RA cost
+    }
+    private static void testSetForList(List list)
+    {
+        int listSize = list.size();
+        Random random = new Random();
+        int reps = 50000;
+        for (int i = 0; i < reps; i ++)
+            list.set(random.nextInt(listSize), "test");
+    }
+
+    private static void testRemoveForList (List list)
+    {
+        while (list.size() > 5)
+            list.remove(5);
     }
 
 }
